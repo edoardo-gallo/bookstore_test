3553e0ad873dbd1256ea0050a88b550d1024d2fc
Edoardo Gallo <edoardogallo5@gmail.com>
2021-09-13 21:26:21 +0200

    initial commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Faker\Factory;
+use App\Models\Author;
+use Illuminate\Http\Request;
+use App\Http\Requests\AuthorRequest;
+use App\Http\Resources\AuthorResource;
+use App\Http\Resources\AuthorsResource;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return AuthorResource::collection(Author::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(AuthorRequest $request)
+    {
+        $faker = Factory::create(1);
+        $author = Author::create([
+            'name' => $faker->name
+        ]);
+
+        return new AuthorResource($author);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Author $author)
+    {
+        return new AuthorResource($author);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+
+     /**  */
+    public function update(AuthorRequest $request, Author $author)
+    {
+        $author->update([
+            'name' => $request->input('name')
+        ]);
+
+        return new AuthorResource($author);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Author $author)
+    {
+        $author->delete();
+
+        return response(null, 204);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RomiuteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->prefix('v1')->group(function() {
+    Route::get('/user', function(Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('/authors', AuthorController::class);
+    Route::apiResource('/books', BookController::class);
+
+    // OR
+
+    // Route::apiResources([
+    //     'photos' => PhotoController::class,
+    //     'posts' => PostController::class,
+    // ]);
+
+});
+
+
+
